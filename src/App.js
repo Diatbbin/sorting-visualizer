2354f9d9e36fdfbd2fb0b09acccdbd85b74c0288
Diatbbin <l_db_@hotmail.com>
2024-05-21 00:20:38 +0800

    Add function to handle sort and insertionSort

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Button
+    </button>
+    </div>
+
   );
 }
 
